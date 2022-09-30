3ec39b2ca3da59c3ef2fabab43675a2204639378
mLn023 <henry.mhilan@gmail.com>
2022-10-01 00:23:43 +0200

    Creation des fixtures et ajout dans la DB

--- src/Controller/Admin/CarCrudController.php
+++ src/Controller/Admin/CarCrudController.php
@@ -7,16 +7,19 @@ use App\Entity\Car;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 
 class CarCrudController extends AbstractCrudController
 {
@@ -25,6 +28,11 @@ class CarCrudController extends AbstractCrudController
         return Car::class;
     }
 
+    public function configureActions(Actions $action): Actions{
+        return $action
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+
+    }
 
     public function configureCrud(Crud $crud): Crud
     {
@@ -40,28 +48,28 @@ class CarCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')                          ->hideOnForm()                      ->hideOnIndex(),
-            TextField::new('Brand')                     ->setLabel('Marque'),
-            TextField::new('Model')                     ->setLabel('Modèle'),
-            AssociationField::new('Category')           ->setLabel('Catégorie'),
-            IntegerField::new('Price')                  ->setLabel('Prix'),
-            TextField::new('imageFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
-            DateField::new('createdAt')                 ->setLabel('Date de création')->hideOnForm(),
-            ImageField::new('imageName')->setBasePath('/images/cars')->onlyOnIndex(),
-            DateField::new('First_Registration')        ->setLabel('Date de mise en circulation')    ->hideOnIndex(),
-            DateField::new('Technical_Control')         ->setLabel('Date du controle tech.')     ->hideOnIndex(),
-            IntegerField::new('Mileage')                ->setLabel('Kilomètrage')               ->hideOnIndex(),
-            TextField::new('Energy')                    ->setLabel('Energie')                ->hideOnIndex(),
-            TextField::new('Gearbox')                   ->setLabel('Boite de vitesse')               ->hideOnIndex(),
-            TextField::new('Color_outside')             ->setLabel('Couleur exterieur')         ->hideOnIndex(),
-            TextField::new('Color_inside')              ->setLabel('Couleur intérieur')          ->hideOnIndex(),
-            IntegerField::new('Num_of_Seats')           ->setLabel('Nombre de sièges')       ->hideOnIndex(),
-            IntegerField::new('Num_of_Doors')           ->setLabel('Nombre de porte')       ->hideOnIndex(),
-            BooleanField::new('isFirstHand')            ->setLabel('Première main?')     ->hideOnIndex(),
-            IntegerField::new('FiscalPower')            ->setLabel('Puissance Fiscale')          ->hideOnIndex(),
-            IntegerField::new('HorsePower')             ->setLabel('Puissance')           ->hideOnIndex(),
-            NumberField::new('CO2_Emission')            ->setLabel('Emission de C.O²')          ->hideOnIndex(),
-            NumberField::new('Consumption')             ->setLabel('Consomation')           ->hideOnIndex()
+            IdField::new('id')                          ->hideOnForm()                                  ->hideOnIndex(),
+            TextField::new('Brand')                                                                     ->setLabel('Marque'),
+            TextField::new('Model')                                                                     ->setLabel('Modèle'),
+            AssociationField::new('Category')                                                           ->setLabel('Catégorie'),
+            MoneyField::new('Price')                    ->setCurrency('EUR')                            ->setLabel('Prix'),
+            TextField::new('imageFile')                 ->setFormType(VichImageType::class)             ->onlyWhenCreating(),
+            DateField::new('createdAt')                 ->setLabel('Date de création')                  ->hideOnForm(),
+            ImageField::new('imageName')                ->setBasePath('/images/cars')                   ->onlyOnIndex(),
+            DateField::new('First_Registration')        ->setLabel('Date de mise en circulation')       ->hideOnIndex(),
+            DateField::new('Technical_Control')         ->setLabel('Date du controle tech.')            ->hideOnIndex(),
+            IntegerField::new('Mileage')                ->setLabel('Kilomètrage')                       ->hideOnIndex(),
+            TextField::new('Energy')                    ->setLabel('Energie')                           ->hideOnIndex(),
+            TextField::new('Gearbox')                   ->setLabel('Boite de vitesse')                  ->hideOnIndex(),
+            TextField::new('Color_outside')             ->setLabel('Couleur exterieur')                 ->hideOnIndex(),
+            TextField::new('Color_inside')              ->setLabel('Couleur intérieur')                 ->hideOnIndex(),
+            IntegerField::new('Num_of_Seats')           ->setLabel('Nombre de sièges')                  ->hideOnIndex(),
+            IntegerField::new('Num_of_Doors')           ->setLabel('Nombre de porte')                   ->hideOnIndex(),
+            BooleanField::new('isFirstHand')            ->setLabel('Première main?')                    ->hideOnIndex(),
+            IntegerField::new('FiscalPower')            ->setLabel('Puissance Fiscale')                 ->hideOnIndex(),
+            IntegerField::new('HorsePower')             ->setLabel('Puissance')                         ->hideOnIndex(),
+            NumberField::new('CO2_Emission')            ->setLabel('Emission de C.O²')                  ->hideOnIndex(),
+            NumberField::new('Consumption')             ->setLabel('Consomation')                       ->hideOnIndex()
         ];
     }
 
